e5035ac0f6b6ebe103bd1ecd51aa6837d8b751a5
EvgenYanukovich <86297837+EvgenYanukovich@users.noreply.github.com>
2022-03-15 22:46:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+$(function() {
+
+    $('.menu-btn').on('click', function() {
+        $('.menu').toggleClass('menu-active');
+    })
+
+    $('#sport').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#sport1').removeClass('content-not-active');
+        $('#sport1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#horoda').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#horoda1').removeClass('content-not-active');
+        $('#horoda1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#school').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#school1').removeClass('content-not-active');
+        $('#school1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#ludi').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#ludi1').removeClass('content-not-active');
+        $('#ludi1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#traditii').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#traditii1').removeClass('content-not-active');
+        $('#traditii1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#fakt').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#fakt1').removeClass('content-not-active');
+        $('#fakt1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#sport-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#sport1').removeClass('content-not-active');
+        $('#sport1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#horoda-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#horoda1').removeClass('content-not-active');
+        $('#horoda1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#school-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#school1').removeClass('content-not-active');
+        $('#school1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#ludi-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#ludi1').removeClass('content-not-active');
+        $('#ludi1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#traditii-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#traditii1').removeClass('content-not-active');
+        $('#traditii1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#fakt-news').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#fakt1').removeClass('content-not-active');
+        $('#fakt1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+
+    $('#glavnoe-menu').click(function() {
+        $('.content-active').addClass('content-not-active');
+        $('.content-active').removeClass('content-active');
+        $('#glavnoe-menu1').removeClass('content-not-active');
+        $('#glavnoe-menu1').addClass('content-active');
+        $('.menu-active').addClass('menu');
+        $('.menu-active').removeClass('menu-active');
+    });
+})
